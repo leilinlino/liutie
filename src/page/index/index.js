1d965afaf350bf04fec43345f05412003f3be270
lei <2742289995@qq.com>
2019-09-16 17:23:15 +0800

    这是第一阶段的代码Day07

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,8 +1,3 @@
-//通过require请求加载cat.js模块
-var cats = require('./cats.js');
-var common = require('../common.js');
+'use strict'
 require('./index.css');
-common();
-//打印请求回来的cats
-console.log(cats);
 $('body').html("羊驼不是草泥马");
